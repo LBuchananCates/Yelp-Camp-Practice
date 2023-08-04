df478a793ea48a361a4aa261c20026ab381288a7
Leighton <leighton@Nicholass-MacBook-Pro.local>
2023-08-04 11:07:18 -0400

    creates basic error handlers

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 // tell express to parse the body; this is for the post req with 😱😱😱
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 // set up method override 🫠🫠🫠
 app.use(methodOverride('_method'));
 
@@ -42,7 +42,7 @@ app.get('/', (req, res) => {
 // diff routes for campgrounds
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
+    res.render('campgrounds/index', { campgrounds })
     // { campgrounds } for index.ejs to work/display!
 })
 
@@ -52,33 +52,37 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 // setting up endpoint as post to which form is submitted 😱😱😱
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    // redirect to newly created campground
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
+        // redirect to newly created campground
+        res.redirect(`/campgrounds/${campground._id}`)
+    } catch (e) {
+        next(e);
+    }
 })
 
 // route for individual campground
 app.get('/campgrounds/:id', async (req, res) => {
     // find by id: pass in the id with req.params.id
     const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', {campground});
+    res.render('campgrounds/show', { campground });
 })
 
 // route for editing
 app.get('/campgrounds/:id/edit', async (req, res) => {
     // looking up cg by id
     const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground});
+    res.render('campgrounds/edit', { campground });
 })
 
 // after method override 🫠🫠🫠
-app.put('/campgrounds/:id', async(req, res) => {
+app.put('/campgrounds/:id', async (req, res) => {
     // this gives us the id
-    const {id} = req.params;
+    const { id } = req.params;
     // second argument is query to update
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     // redirect to campground showpage of campground just updated
     res.redirect(`/campgrounds/${campground._id}`)
 });
@@ -86,11 +90,16 @@ app.put('/campgrounds/:id', async(req, res) => {
 // delete route; needs id included
 app.delete('/campgrounds/:id', async (req, res) => {
     // take id to destructure it
-    const {id} = req.params;
+    const { id } = req.params;
     // to delete: find info about cg and remove
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
     // then make button to send delete request, go to showpage
+})
+
+// error handlers MUST follow all async functions
+app.use((err, req, res, next) => {
+    res.send('Oh boy, something went wrong!')
 })
 
 app.listen(3000, () => {
